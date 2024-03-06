85bbf30b51af28ba150aea5932cae46953e131a5
Kyrylo <kirill.shelydchenko@gmail.com>
2024-03-06 18:59:42 +0200

    Добавьте файлы проекта.

---
+++ Homework_1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Homework_1
+{
+    using System;
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter price of pens:");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter price of copybooks:");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter AMOUT of pens:");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter AMOUT of copybooks:");
+            int m = int.Parse(Console.ReadLine());
+
+            int sum = (n * x) + (m * y);
+
+            Console.WriteLine($"Total Price: {sum}");
+        }
+    }
+}
